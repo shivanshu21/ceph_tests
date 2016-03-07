76e92f8052609a0b11a50edca0b68490d5455ac8
Shivanshu Goswami <shivanshu21@gmail.com>
2016-03-07 08:56:51 +0000

    latest pcurl

--- curl/pcurl.pl
+++ curl/pcurl.pl
@@ -3,20 +3,29 @@
 ## DSS main
 #my $user_id        = '08d9079e37ca4750ae1b223af8869f92';
 #my $user_name      = 'DSS';
-#my $passw          = 'Reliance111@';
+#my $passw          = '';
+#my $access_key     = '';
+#my $signature      = 'H/AUlsWSTz7LeeeSDhu2G4m8S+E=';
+#my $cannonical_str = 'R0VUCgoKRnJpLCAwNSBGZWIgMjAxNiAxMTowNjo0NiBHTVQKLw==';
 
 ## dss_test
 #my $user_id        = '304018976221';
 #my $user_name      = 'dss_test_0001';
 #my $passw          = 'D2=lH6-fA2$b';
+#my $access_key     = '';
+#my $signature      = '';
+#my $cannonical_str = '';
+
 
 ## Console account
 my $user_id         = '652281179750';
 my $user_name       = 'console_account';
-my $passw           = '1PaQ3TQK706jx7iA9Dy5EA==';
+my $passw           = 'eblLLz4pj+pPyczEzAs9ZQ==';
+my $access_key      = '1376e515a13b459c9e390612b855afc1';
+#my $signature      = '';
+#my $cannonical_str = '';
 
-my $token          = '3caf1cff673e4709b890ddb84b79150b'; ## Generate and put
-my $access_key     = '2bf9f42cc7fa47fdae06f14dddd7e4ac'; ## DSS main. Needed for sign req
+my $token          = '8548e7ac9889404c839d89ef9b7b539e_8548e7ac9889404c839d89ef9b7b539e'; ## Generate and put
 my $user_to_create = ''; ## For creating new users
 ##===================================
 
@@ -26,11 +35,9 @@ my $user_to_create = ''; ## For creating new users
 ##===================================
 
 ## No terminating slash '/' in URLs
-#my $rgw_endpoint   = 'http://127.0.0.1:8000';
-my $rgw_endpoint   = 'http://10.140.214.198:7480';
+#my $rgw_endpoint  = 'http://127.0.0.1:8000';
+my $rgw_endpoint   = 'http://10.140.214.196:7480';
 my $iam_endpoint   = 'https://iam.ind-west-1.staging.jiocloudservices.com:5000/v3';
-my $signature      = 'H/AUlsWSTz7LeeeSDhu2G4m8S+E=';
-my $cannonical_str = 'R0VUCgoKRnJpLCAwNSBGZWIgMjAxNiAxMTowNjo0NiBHTVQKLw==';
 our $GLOBAL_DEBUG  = 1;
 
 ##===================================
@@ -39,9 +46,9 @@ our $GLOBAL_DEBUG  = 1;
 
 my $signreq = "curl -s -vvv -X POST $iam_endpoint/sign-auth -H \"Content-Type: application/json\" -d '{\"credentials\": {\"access\": \"$access_key\", \"signature\": \"$signature\", \"token\": \"$cannonical_str\", \"action_resource_list\": [{\"action\": \"jrn:jcs:dss:ListBucket\", \"resource\": \"jrn:jcs:dss::Bucket:*\", \"implicit_allow\": \"False\"}]}}'";
 
-my $tokreq = "curl -v -H \"Content-Type: application/json\" -d '{ \"auth\": {\"identity\": {\"methods\": [\"password\"],\"password\": {\"user\": {\"name\": \"$user_name\",\"account\": { \"id\": \"$user_id\" },\"password\":\"$passw\",\"access\":\"9dce6aa7c5554521b807aa343fc948ce\"}}}}}' $iam_endpoint/auth/tokens";
+my $tokreq = "curl -i -H \"Content-Type: application/json\" -d '{ \"auth\": {\"identity\": {\"methods\": [\"password\"],\"password\": {\"user\": {\"name\": \"$user_name\",\"account\": { \"id\": \"$user_id\" },\"password\": \"eblLLz4pj+pPyczEzAs9ZQ==\",\"access\":\"$access_key\"}}}}}' $iam_endpoint/auth/tokens";
 
-my $usercreate = "curl -i -H \"Content-Type: application\/json\" -H \"X-Auth-Token: $token\"  \"$iam_endpoint?Action=CreateUser&Name=$user_to_create&Password=Reliance111@\" ";
+my $usercreate = "curl -i -H \"Content-Type: application/json\" -H \"X-Auth-Token: $token\" \"$iam_endpoint?Action=CreateUser&Name=$user_to_create&Password=Reliance111@\" "; #This passwd is just the default pass of new user
 
 my $usercredentialscreate = "curl -i -H \"Content-Type: application\/json\" -H \"X-Auth-Token: $token\"  $iam_endpoint?Action=CreateCredential&UserId=$user_id";
 
@@ -51,35 +58,35 @@ my $iamtokvalidation = "curl -v -H \"X-Auth-Token: $token\" \"$iam_endpoint/toke
 ## DSS REQUESTS
 ##===================================
 
-#my $rgwreq = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" $rgw_endpoint/consolebucktest001"; # Create bucket
+my $rgwreqcb = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" $rgw_endpoint/cobucket002"; # Create bucket
 
-#my $rgwreq = "curl -v -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # List bucket
+my $rgwreqlb = "curl -v -H \"X-Auth-Token: $token\" $rgw_endpoint/cobucket002"; # List bucket
 
-#my $rgwreq = "curl -v -H \"X-Auth-Token: $token\" $rgw_endpoint/"; # List all my buckets
+my $rgwreqlab = "curl -v -H \"X-Auth-Token: $token\" $rgw_endpoint/"; # List all my buckets
 
-#my $rgwreq = "curl -v -X \"HEAD\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # Head bucket
+my $rgwreqhb = "curl -v -X \"HEAD\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # Head bucket
 
-#my $rgwreq = "curl -v -X \"DELETE\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # Delete bucket
+my $rgwreqdb = "curl -v -X \"DELETE\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # Delete bucket
 
-#####my $rgwreq = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # Create object
+my $rgwreqco = "curl -v -X \"PUT\" -T fil -H \"X-Auth-Token: $token\" $rgw_endpoint/cobucket001/obj"; # Create object
 
-#my $rgwreq = "curl -v -X \"GET\" -H \"X-Auth-Token: $token\" $rgw_endpoint/bucket1/obj"; # Download object
+my $rgwreqlo = "curl -v -X \"GET\" -H \"X-Auth-Token: $token\" $rgw_endpoint/cobucket002?acl"; # Download object
 
-#my $rgwreq = "curl -v -X \"HEAD\" -H \"X-Auth-Token: $token\" \"$rgw_endpoint/bucket1/obj\""; # Head object
+my $rgwreqho = "curl -v -X \"HEAD\" -H \"X-Auth-Token: $token\" \"$rgw_endpoint/bucket1/obj\""; # Head object
 
-#my $rgwreq = "curl -v -X \"DELETE\" -H \"X-Auth-Token: $token\" $rgw_endpoint/bucket1/obj"; # Delete object
+my $rgwreqdo = "curl -v -X \"DELETE\" -H \"X-Auth-Token: $token\" $rgw_endpoint/bucket1/obj"; # Delete object
 
-#my $rgwreq = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" -H \"x-jcs-metadata-directive: COPY\" -H \"x-jcs-copy-source: copysourcebucket\/obj500\" -H \"x-jcs-storage-class: STANDARD\" -H \"Content-Length: 0\" \"$rgw_endpoint/shivbucket0002/obj501\""; # Copy object
+my $rgwreqpo = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" -H \"x-jcs-metadata-directive: COPY\" -H \"x-jcs-copy-source: copysourcebucket\/obj500\" -H \"x-jcs-storage-class: STANDARD\" -H \"Content-Length: 0\" \"$rgw_endpoint/shivbucket0002/obj501\""; # Copy object
 
-#my $rgwreq = "curl -v -X \"POST\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002/output.dat?uploads"; # Initiate multipart upload
+my $rgwreqimu = "curl -v -X \"POST\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002/output.dat?uploads"; # Initiate multipart upload
 
-####my $rgwreq = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002output.dat?uploadId=2~0qEl87oQBALdIZY-g6IcawZuKjyLFi3&partNumber=1"; # Upload a part
+####my $rgwreqmup = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002output.dat?uploadId=2~0qEl87oQBALdIZY-g6IcawZuKjyLFi3&partNumber=1"; # Upload a part
 
-#my $rgwreq = "curl -v -X \"GET\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002?uploadId=2~D3L6Qv_VdSER9dhOs2BjTkKXSTGcxPU"; # List all parts
+my $rgwreqmpart = "curl -v -X \"GET\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002?uploadId=2~D3L6Qv_VdSER9dhOs2BjTkKXSTGcxPU"; # List all parts
 
-#my $rgwreq = "curl -v -X \"GET\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002?uploads"; # List all multipart uploads
+my $rgwreqmall = "curl -v -X \"GET\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002?uploads"; # List all multipart uploads
 
-#my $rgwreq = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # Abort multipart uploads
+my $rgwreqmd = "curl -v -X \"PUT\" -H \"X-Auth-Token: $token\" $rgw_endpoint/shivbucket0002"; # Abort multipart uploads
 
 ##===================================
 ## WORK FLOW
